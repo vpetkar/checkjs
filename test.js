1b6af7302cb9cf65d01c53c35b758c5b32c938f6
Ved Petkar <vedpetkar@gmail.com>
2016-06-01 19:15:27 -0400

    exports email

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 var check = require('./lib/check.js');
 
 console.log("Sanitized input:" + check.email("yOLOOOOO@gmail.com"));
+console.log("Sanitized input:" + check.name("Ved petkarDDDF pdf12 "));
